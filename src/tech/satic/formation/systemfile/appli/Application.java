046814e4f004701b5a4874f5315ad6bcf4e488a2
lorincet <lorincet@gmail.com>
2021-11-23 20:01:27 +0100

    Initial commit

---
+++ src/tech/satic/formation/systemfile/appli/Application.java
@@ -0,0 +1,18 @@
+// Package
+package tech.satic.formation.systemfile.appli;
+
+// Librairie
+
+import java.util.Arrays;
+
+/**
+ * @author Lorince TAWAMBA
+ * @version 1.0.0
+ * @since 23/11/2021
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("==== Start of the program ====");
+    }
+}
